bde2dcfe7b4fba678f70adf0bd2999616a78a260
raisinware <102261580+raisinware@users.noreply.github.com>
2023-02-28 18:50:09 -0800

    switch to C version of echo
    might need to check if manually locking stdout is needed

---
+++ usr/bin/echo/echo.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: Apache-2.0 OR ISC */
+/* echo.c - print all arguments and end with a newline */
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+    // iterate through most arguments and print to stdout.
+	for ( int i=1; i < argc; i++ ) {
+        fputs(argv[i], stdout);
+
+        // print space if not the last argument
+        if (i < (argc-1))
+            fputc(' ', stdout);
+    }
+
+    // print newline
+	puts("");
+
+    return 0;
+}
