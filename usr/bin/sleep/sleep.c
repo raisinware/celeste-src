7be6f021b2aa87e2582bf3e56863a8ccb0bb14df
raisinware <102261580+raisinware@users.noreply.github.com>
2023-03-12 14:37:37 -0700

    rewrite sleep in c
    all the c++ code is gone already x3

---
+++ usr/bin/sleep/sleep.c
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: Apache-2.0 OR ISC */
+/* sleep.c - sleep for x amount of seconds */
+
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <unistd.h>
+
+/** Display usage message and exit with an error */
+static noreturn void usage(void){
+	fprintf(stderr, "usage: %s [seconds]\n",
+		program_invocation_short_name);
+	exit(EXIT_FAILURE);
+}
+
+/** Convert a string into an unsigned int with strict error checking */
+unsigned int getuint(const char* str){
+	/* reset errno to 0 incase something else set it */
+	errno = 0;
+
+	char* leftover = NULL;
+	unsigned int secs = strtoul(str, &leftover, 10);
+
+	/* check that a valid number was fed with no trailing characters */
+	if ( (errno == 0 && str && !*leftover) && (str != leftover) )
+		return secs;
+	else usage();
+}
+
+int main(int argc, char* argv[]){
+	/* check that only 1 argument was passed */
+	if ( argc != 2 ) 
+		usage();
+
+	sleep(getuint(argv[1]));
+
+	return EXIT_SUCCESS;
+}
