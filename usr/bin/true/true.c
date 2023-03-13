7536de25b444a59c273a52d0c39b2557f05ddf40
raisinware <102261580+raisinware@users.noreply.github.com>
2023-03-12 18:43:19 -0700

    remove unneeded copyright boiler text

--- usr/bin/true/true.c
+++ usr/bin/true/true.c
@@ -1,19 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 OR ISC */
-
-/* true.c - return an exit code of 0
- *
- * Copyright (c) 2023 by raisinware
- * 
- * Licensed under either the Apache-2.0 or ISC Licenses, at your option.
- * You may obtain a copy of each license at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *     https://github.com/raisinware/celeste-src/blob/main/LICENSE.ISC
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
+/* true.c - return an exit code of 0 */
 
 int main() {
 	return 0;
