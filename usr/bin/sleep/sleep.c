99cca04f4ce44c9024bb549aa7753da755e061f7
raisinware <102261580+raisinware@users.noreply.github.com>
2023-03-19 11:52:28 -0700

    sleep cleanups
    switch to libutil for progname, remove _GNU_SOURCE feature macro, and switch to `//` style comments

--- usr/bin/sleep/sleep.c
+++ usr/bin/sleep/sleep.c
@@ -1,8 +1,8 @@
-/* SPDX-License-Identifier: Apache-2.0 OR ISC */
-/* sleep.c - sleep for x amount of seconds */
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+// sleep.c - sleep for x amount of seconds
 
-#define _GNU_SOURCE
 #include "../../include/CelesteC.h"
+#include "../../lib/util/CelesteUtil.h"
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
@@ -10,29 +10,30 @@
 #include <string.h>
 #include <unistd.h>
 
-/** Display usage message and exit with an error */
+/// Display usage message and exit with an error
 noreturn static void usage(void){
 	fprintf(stderr, "usage: %s [seconds]\n",
-		program_invocation_short_name);
+		getprogname());
 	exit(EXIT_FAILURE);
 }
 
-/** Convert a string into an unsigned int with strict error checking */
+/// Convert a string into an unsigned int with strict error checking
 unsigned int getuint(const char* str){
-	/* reset errno to 0 incase something else set it */
+	// reset errno to 0 incase something else set it
 	errno = 0;
 
 	char* leftover = NULL;
 	unsigned long secs = strtoul(str, &leftover, 10);
 
-	/* check that a valid number was fed with no trailing characters */
+	// check that a valid number was fed with no trailing characters
 	if ( (errno == 0 && str && !*leftover) && (str != leftover) && (secs <= UINT_MAX) )
 		return secs;
-	else usage();
+	else
+		usage();
 }
 
 int main(int argc, char* argv[]){
-	/* check that only 1 argument was passed */
+	// check that only 1 argument was passed
 	if ( argc != 2 ) 
 		usage();
 
