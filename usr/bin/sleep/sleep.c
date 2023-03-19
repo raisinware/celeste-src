b0657d9a7ffb14d26753870219cc5259cc1a32af
raisinware <102261580+raisinware@users.noreply.github.com>
2023-03-19 16:43:58 -0700

    Update sleep.c
    now uses strtonum from libutil :3

--- usr/bin/sleep/sleep.c
+++ usr/bin/sleep/sleep.c
@@ -3,41 +3,31 @@
 
 #include "../../include/CelesteC.h"
 #include "../../lib/util/CelesteUtil.h"
-#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 /// Display usage message and exit with an error
-noreturn static void usage(void){
+noreturn static void usage(void) {
 	fprintf(stderr, "usage: %s [seconds]\n",
 		getprogname());
 	exit(EXIT_FAILURE);
 }
 
-/// Convert a string into an unsigned int with strict error checking
-unsigned int getuint(const char* str){
-	// reset errno to 0 incase something else set it
-	errno = 0;
+int main(int argc, char* argv[]) {
+	const char* errstr;
 
-	char* leftover = NULL;
-	unsigned long secs = strtoul(str, &leftover, 10);
-
-	// check that a valid number was fed with no trailing characters
-	if ( (errno == 0 && str && !*leftover) && (str != leftover) && (secs <= UINT_MAX) )
-		return secs;
-	else
-		usage();
-}
-
-int main(int argc, char* argv[]){
 	// check that only 1 argument was passed
 	if ( argc != 2 ) 
 		usage();
+	
+	unsigned int num = strtonum(argv[1], 0, UINT_MAX, &errstr);
 
-	sleep(getuint(argv[1]));
+	if (errstr != NULL)
+		usage();
+	else
+	 	sleep(num);
 
 	return EXIT_SUCCESS;
 }
