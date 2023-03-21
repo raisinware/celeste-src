6d960c3cb6305d5d329537633a83895c7e11c882
raisinware <102261580+raisinware@users.noreply.github.com>
2023-03-21 16:23:07 -0700

    clean up awk buildprocess
    should now be cross compiling friendly :3

--- usr/bin/awk/maketab.sh
+++ usr/bin/awk/maketab.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-"$1/usr/bin/awk/_maketab" "$1/usr/bin/awk/awkgram.tab.h" >proctab.c
+# SPDX-License-Identifier: Apache-2.0 OR ISC
+# maketab.sh - generate proctab.c
+set -x
+set -e
+
+CC="cc"
+YACC="yacc"
+
+AWK_SRCDIR="../../../extern/permissive/one-true-awk"
+
+$YACC -d -b awkgram "${AWK_SRCDIR}/awkgram.y"
+rm awkgram.tab.c
+$CC -I. -o maketab "${AWK_SRCDIR}/maketab.c"
+
+./maketab awkgram.tab.h > proctab.c
+rm awkgram.tab.h
+rm maketab
