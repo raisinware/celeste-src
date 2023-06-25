6664a96fdc7dc93ae3a5249a188451a0a49fc633
raisinware <102261580+raisinware@users.noreply.github.com>
2023-06-24 21:35:14 -0700

    libutil: move macros to top of header
    looks cleaner :3

--- usr/lib/util/CelesteUtil.h
+++ usr/lib/util/CelesteUtil.h
@@ -14,14 +14,14 @@
 #include "../../include/CelesteC.h"
 #include <string.h>
 
-/// Get the name of the current program
-CELESTE_PUBLIC __reproducible const char* getprogname(void);
-
 /// no-op macro for bsd compat
 #define setprogname(x) do {} while (0)
 
 /// Determine if 2 strings are equal. Returns true if equal, and false if not.
 #define STREQ(str1, str2) (strcmp(str1, str2) == 0)
+
+/// Get the name of the current program
+CELESTE_PUBLIC __reproducible const char* getprogname(void);
 
 /// Convert a string into a number with error checking
 CELESTE_PUBLIC long long strtonum(const char* str, long long minval,
